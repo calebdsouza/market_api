01b0cb848bd610ca9ee034003b706f427ee8f1d6
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-11 15:23:40 -0500

    Created new initial GraphQL Mutation Function for creating a new User

---
+++ app/graphql/mutations/create_user.rb
@@ -0,0 +1,30 @@
+# Mutation resolver to create a new User
+class Mutations::CreateUser < GraphQL::Function
+    # Define a new GraphQL input object type for new user authentication
+    AuthProviderInput = GraphQL::InputObjectType.define do
+        name 'AuthProviderSignupData'
+
+        argument : email, Types::AuthProviderEmailInput
+    end
+
+    # Arguments passed as 'args' needed tp create a new User
+    argument :name, !types.String
+    argumnet :authProvider, !AuthProviderInput
+
+    # Return type for the mutation of User node
+    type Types:: UserType
+
+    # The mutation method for creating a new User
+    # _obj - parent object, which in this case is nil
+    # args - arguments passed (name, email, password)
+    # _ctx - GraphQL Context
+    def call(_obj, args, ctx)
+        # Create new User
+        User.create!(
+            name: args[:name],
+            email: args[:authProvider][:email][:email],
+            password: args[:authProvider][:email][:password]
+        )
+    end
+end
+ 
