b17e9a544f11754df6b7773daef513221e0f4909
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-11 15:53:28 -0500

    Created inital unit test for User creation Mutation

---
+++ test/graphql/mutations/create_user_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class Mutations::CreateUserTest < ActiveSupport::TestCase
+    def preform(args = {})
+        Mutations::CreateUser.call(nil, args, nil)
+    end
+
+    # Test creating a standard vaild User
+    test 'creating new user' do
+        user = preform(
+            name: 'Test User',
+            authProvider: {
+                email: {
+                    email: 'myTestEmail@example.com',
+                    password: '[omitted]'
+                }
+            }
+        )
+
+        assert user.persisted?
+        assert_equal user.name, 'Test User'
+        assert_equal user.email, 'myTestEmail@example.com'
+    end
+end
