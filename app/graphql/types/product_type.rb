ef82ea2ecc9246dbbb01c7df9c5e9f5f45d8d111
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-10 19:37:26 -0500

    Modified comments

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -1,9 +1,9 @@
-# Defines new GraphQL type named Product
+# Defines new GraphQL type, ProductType
 Types::ProductType = GraphQL::ObjectType.define do
     # Set this type's name to 'Product'
     name 'Product'
 
-    # Define the following feilds for the product GraphQL type, Product
+    # Define the following feilds for the Product GraphQL type
     field :id, !types.ID
     field :title, !types.String
     field :price, !types.Float
