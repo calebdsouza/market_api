1542b90d3e7ebee9eefd16b3f769e4429167dbe3
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-12 19:19:02 -0500

    Created a new Cart node in the db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_154909) do
+ActiveRecord::Schema.define(version: 2019_01_12_204858) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
