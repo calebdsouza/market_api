1a0aaad74482c6f51c2181740896e95e56b460d4
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-12 21:50:21 -0500

    Updated cart to have total price, and product count properties

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,3 @@
 class Cart < ApplicationRecord
-  belongs_to :product, validate: true
   belongs_to :user, validate: true
 end
--- db/migrate/20190112204858_create_carts.rb
+++ db/migrate/20190112204858_create_carts.rb
@@ -1,8 +1,9 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-      t.references :product, foreign_key: true
       t.references :user, foreign_key: true
+      t.decimal :total_price
+      t.integer :product_count
 
       t.timestamps
     end
