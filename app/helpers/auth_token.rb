f011e9fd709d2ba2e2b805b61c4171c80b5a060b
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-12 19:12:47 -0500

    Updated secret_key_base version and fixed invalid shared key error

--- app/helpers/auth_token.rb
+++ app/helpers/auth_token.rb
@@ -1,21 +1,21 @@
 class AuthToken
 
     # Generate sercet method
-    def self.getSecret
-        Rails.application.secrets.secret_key_base
+    def self.key
+        Rails.application.credentials.secret_key_base.byteslice(0..31)
     end
 
     # Generate JWT token method using default HS256 hash
     # user - the user to generate the token for
     def self.issue(user)
         payload = {user_id: user.id}
-        JsonWebToken.sign(payload, key: getSecret)
+        JsonWebToken.sign(payload, key: key)
     end
 
     # Verification of given JWT using dedault HS256 hash
     # token - the token to be decoded and verified
     def self.verify(token)
-        result = JsonWebToken.verify(token, key: getSecret)
+        result = JsonWebToken.verify(token, key: key)
         return nil if result[:error]
         user.find_by(id: result[:ok][:user_id])
     end
