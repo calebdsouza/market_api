5ecadee61a97f37db19c65e5eb40fa4bb623e7e6
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-13 21:12:49 -0500

    Renamed file

--- app/graphql/types/auth_provider_email_input.rb
+++
@@ -1,9 +0,0 @@
-# Define new GraphQL input type, 'AUTH_PROVIDER_EMAIL'
-# for the authentication provider 
-Types::AuthProviderEmailInput = GraphQL::InputObjectType.define do
-    name 'AUTH_PROVIDER_EMAIL'
-
-    # Required arguments passed as 'args' needed to authenticate
-    argument :email, !types.String
-    argument :password, !types.String
-end
