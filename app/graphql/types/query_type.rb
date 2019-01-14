a3a4feae778fe3bbfb17e42052576270ed8f1aa0
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-13 21:22:44 -0500

    Added get all user and get current user to graphql query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,14 +1,25 @@
 # Define a new GraphQL type, Query
-
 Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
-  # Add root-level fields here.
-  # They will be entry points for queries on your schema.
   field :allProducts, !types[Types::ProductType] do
     description "Resolve all products in this database"
     resolve -> (obj, args, ctx) {
       Product.all
+    }
+  end
+
+  field :allUsers, !types[Types::UserType] do
+    description "Resolve all users in this database"
+    resolve -> (obj, args, ctx) {
+      User.all
+    }
+  end
+
+  field :currentUser, Types::UserType do
+    description "Resovle the current signed in user from this database"
+    resolve -> (obj, args, ctx) {
+      User.find_by(ctx[:current_user])
     }
   end
 
