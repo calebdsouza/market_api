078b2d51be1c2b3bed41366d833240a13dc119db
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-13 21:14:15 -0500

    Exposed new create empty cart mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,10 +8,12 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :createProduct, function: Mutations::CreateProduct.new
   # User Mutations
     # Create new User
-  field :createUser, function: Mutation::CreateUser.new
+  field :createUser, function: Mutations::CreateUser.new
     # Sign in User
-  field :signinUser, function: Mutation::SignInUser.new
-  
+  field :signinUser, function: Mutations::SignInUser.new
+  # Cart Mutations
+    # Create new empty Cart
+  field :createCart, function: Mutations::CreateCart.new
   # TODO: Remove me
   # field :testField, types.String do
   #   description "An example field added by the generator"
