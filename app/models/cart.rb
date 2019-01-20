2dfd9e930b1590a40edd8d5779aab966db3fec53
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-20 14:56:17 -0500

    Update to include a new Product field by consquence of adding cart-product table

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,5 @@ class Cart < ApplicationRecord
   belongs_to :user, validate: true
   validates :total_price, presence: true
   validates :product_count, presence: true
+  has_and_belongs_to_many :products, presence: true
 end
