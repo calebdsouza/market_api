e27df52ce7149354bff14dc90b283ae8ce1b2477
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-20 14:55:04 -0500

    Update to include a new Cart field by consquence of adding cart-product table

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
-    validates :title, presence: true, uniqueness: true
+    validates :title, presence: true, uniqueness: true, length: {minimum: 3}
     validates :price, presence: true
     validates :inventory_count, presence: true
+    has_and_belongs_to_many :carts
 end
