b0f4050248ea5128dc14a213ca117d6f8da100d4
CD <cd@DFamilys-MacBook-Pro.local>
2019-01-20 15:49:21 -0500

    Updated to include all carts, all products in User's Cart, and current User querys

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -16,10 +16,24 @@ Types::QueryType = GraphQL::ObjectType.define do
     }
   end
 
-  field :currentUser, Types::UserType do
+  field :allCarts, !types[Types::CartType] do
+    description "Resovle all carts in this database"
+    resolve -> (obj, args, ctx) {
+      Cart.all
+    }
+  end
+
+  field :allCartsProducts, !types[Types::CartsProductsType] do
+    description "Resovle all products in a cart in this database"
+    resolve -> (obj, args, ctx) {
+      CartsProducts.all
+    }
+  end
+
+  field :currentUser, !Types::UserType do
     description "Resovle the current signed in user from this database"
     resolve -> (obj, args, ctx) {
-      User.find_by(ctx[:current_user])
+      ctx[:current_user]
     }
   end
 
